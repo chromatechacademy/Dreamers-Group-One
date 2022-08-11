2ddee3267d1fe956da66999f272733174710fba7
Vicooui <106284742+Vicooui@users.noreply.github.com>
2022-08-11 10:19:49 -0400

    modules

--- src/test/java/com/pages/CTMSHomePage.java
+++ src/test/java/com/pages/CTMSHomePage.java
@@ -2,12 +2,19 @@ package com.pages;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.web.WebDriverUtils;
 
 public class CTMSHomePage {
 
       /** Navigation Modules */
-      @FindBy(xpath = "//section[@class='sidebar']")
-      public WebElement navigationModules;
+      @FindBy(xpath = "//span[normalize-space()='Student Information']")
+      public  WebElement navigationModules;
+
+      public CTMSHomePage(){
+        PageFactory.initElements(WebDriverUtils.driver, this);
+      }
   
     
 }
--- src/test/java/com/stepDefinitions/Modules.java
+++ src/test/java/com/stepDefinitions/Modules.java
@@ -2,10 +2,12 @@ package com.stepDefinitions;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 
 import com.constants.ApplicationConstants;
 import com.pages.CTMSHomePage;
 import com.pages.LoginPage;
+import com.web.CommonUtils;
 import com.web.WebDriverUtils;
 
 import cucumber.api.java.en.Given;
@@ -14,6 +16,7 @@ import cucumber.api.java.en.Then;
 public class Modules {
     LoginPage loginPage = new LoginPage();
     CTMSHomePage ctmsHomePage = new CTMSHomePage();
+
 
     @Given("user has logged in with valid credentials")
     public void user_has_logged_in_with_valid_credentials() {
@@ -26,10 +29,10 @@ public class Modules {
 
     @Then("the navigation modules should be displayed")
     public void the_navigation_modules_should_be_displayed() {
-        
-        ctmsHomePage.navigationModules.getText();
 
-
+        String ActualModules = ctmsHomePage.navigationModules.getText();
+        String ExpectedModules = "Student Information";
+        Assert.assertEquals(ActualModules, ExpectedModules);
 
     }
 }
