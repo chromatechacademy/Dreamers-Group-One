605f6b1f9fe0c131036e935397f724a601ac10fb
Vicooui <106284742+Vicooui@users.noreply.github.com>
2022-08-11 15:50:12 -0400

    NavigationModules Completed

--- src/test/java/com/pages/CTMSHomePage.java
+++ src/test/java/com/pages/CTMSHomePage.java
@@ -8,13 +8,40 @@ import com.web.WebDriverUtils;
 
 public class CTMSHomePage {
 
-      /** Navigation Modules */
-      @FindBy(xpath = "//span[normalize-space()='Student Information']")
-      public  WebElement navigationModules;
+  /** Student Information Module */
+  @FindBy(xpath = "//span[normalize-space()='Student Information']")
+  public WebElement studentInfoModule;
 
-      public CTMSHomePage(){
-        PageFactory.initElements(WebDriverUtils.driver, this);
-      }
-  
-    
+  /* Fees Collection Module */
+  @FindBy(xpath = "//span[normalize-space()='Fees Collection']")
+  public WebElement feesCollectionModule;
+
+  /* Income Module */
+  @FindBy(xpath = "//span[normalize-space()='Income']")
+  public WebElement incomeModule;
+
+  /* Expenses Module */
+  @FindBy(xpath = "//span[normalize-space()='Expenses']")
+  public WebElement expensesModule;
+
+  /* Academic Module */
+  @FindBy(xpath = "//span[normalize-space()='Academics']")
+  public WebElement academicModule;
+
+  /* Human Resource Module */
+  @FindBy(xpath = "//span[normalize-space()='Human Resource']")
+  public WebElement humanResourceModule;
+
+  /* Homework Module */
+  @FindBy(xpath = "//span[normalize-space()='Homework']")
+  public WebElement homeworkModule;
+
+  /* Reports Module */
+  @FindBy(xpath = "//span[normalize-space()='Reports']")
+  public WebElement reportsModule;
+
+  public CTMSHomePage() {
+    PageFactory.initElements(WebDriverUtils.driver, this);
+  }
+
 }
--- src/test/java/com/stepDefinitions/Modules.java
+++ src/test/java/com/stepDefinitions/Modules.java
@@ -1,5 +1,7 @@
 package com.stepDefinitions;
 
+import java.io.IOException;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
@@ -7,6 +9,7 @@ import org.testng.Assert;
 import com.constants.ApplicationConstants;
 import com.pages.CTMSHomePage;
 import com.pages.LoginPage;
+import com.utils.CucumberLogUtils;
 import com.web.CommonUtils;
 import com.web.WebDriverUtils;
 
@@ -16,7 +19,6 @@ import cucumber.api.java.en.Then;
 public class Modules {
     LoginPage loginPage = new LoginPage();
     CTMSHomePage ctmsHomePage = new CTMSHomePage();
-
 
     @Given("user has logged in with valid credentials")
     public void user_has_logged_in_with_valid_credentials() {
@@ -28,11 +30,41 @@ public class Modules {
     }
 
     @Then("the navigation modules should be displayed")
-    public void the_navigation_modules_should_be_displayed() {
+    public void the_navigation_modules_should_be_displayed() throws IOException {
 
-        String ActualModules = ctmsHomePage.navigationModules.getText();
-        String ExpectedModules = "Student Information";
-        Assert.assertEquals(ActualModules, ExpectedModules);
+        CommonUtils.waitForVisibility(ctmsHomePage.studentInfoModule);
+        String ActualStudentModules = ctmsHomePage.studentInfoModule.getText();
+        String ExpectedStudentModules = "Student Information";
+        Assert.assertEquals(ActualStudentModules, ExpectedStudentModules);
+
+        String ActualFeesModule = ctmsHomePage.feesCollectionModule.getText();
+        String ExpectedFeesModule = "Fees Collection";
+        Assert.assertEquals(ActualFeesModule, ExpectedFeesModule);
+        CucumberLogUtils.logExtentScreenshot();
+
+        String ActualIncomeModule = ctmsHomePage.incomeModule.getText();
+        String ExpectedIncomeModule = "Income";
+        Assert.assertEquals(ActualIncomeModule, ExpectedIncomeModule);
+
+        String ActualExpenseModule = ctmsHomePage.expensesModule.getText();
+        String ExpectedExpenseModule = "Expenses";
+        Assert.assertEquals(ActualExpenseModule, ExpectedExpenseModule);
+
+        String ActualAcademicModule = ctmsHomePage.academicModule.getText();
+        String ExpectedAcademicModule = "Academics";
+        Assert.assertEquals(ActualAcademicModule, ExpectedAcademicModule);
+
+        String ActualHumanResourceModule = ctmsHomePage.humanResourceModule.getText();
+        String ExpectedHumanResourceModule = "Human Resource";
+        Assert.assertEquals(ActualHumanResourceModule, ExpectedHumanResourceModule);
+
+        String ActualHomeworkModule = ctmsHomePage.homeworkModule.getText();
+        String ExpectedHomeworkModule = "Homework";
+        Assert.assertEquals(ActualHomeworkModule, ExpectedHomeworkModule);
+
+        String ActualReportsModule = ctmsHomePage.reportsModule.getText();
+        String ExpectedReportsModule = "Reports";
+        Assert.assertEquals(ActualReportsModule, ExpectedReportsModule);
 
     }
 }
