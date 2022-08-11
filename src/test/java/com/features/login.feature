76ea7d44e743090ce38d574e2912bc6da2bb965c
Jomir Uddin <jomir.uddin112@gmail.com>
2022-08-11 14:42:12 -0400

    Verified successful login

--- src/test/java/com/features/login.feature
+++ src/test/java/com/features/login.feature
@@ -7,7 +7,7 @@ Feature: Login Scenarios
     Scenario: Login with valid credentials
         Given a teacher is on the CTSMS login page
         When the teacher logs in with username and password
-        Then the teacher should be succesfully logged in
+        Then the teacher should be succesfully logged in and page title is "Chroma Tech Academy"
 
     # @jomir @Smoke
     # Scenario: Invalid Credentials login attempt
--- src/test/java/com/stepDefinitions/LoginStepDef.java
+++ src/test/java/com/stepDefinitions/LoginStepDef.java
@@ -8,11 +8,13 @@ import com.constants.ApplicationConstants;
 import com.pages.LoginPage;
 import com.utils.CucumberLogUtils;
 import com.utils.ScreenShots;
+import com.web.CommonUtils;
 import com.web.WebDriverUtils;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import cucumber.api.junit.Cucumber;
 
 public class LoginStepDef {
 
@@ -30,22 +32,25 @@ public class LoginStepDef {
         loginPage.passwordTextBox.sendKeys(ApplicationConstants.PASSWORD);
         loginPage.signInButton.click();
         try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
+            // Thread.sleep(3000);
+            CommonUtils.getWaitObject();
+        } catch (Exception e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
 
-    @Then("the teacher should be succesfully logged in")
-    public void the_teacher_should_be_succesfully_logged_in() {
-        
+    @Then("the teacher should be succesfully logged in and page title is {string}")
+    public void the_teacher_should_be_succesfully_logged_in_and_page_title_is(String EXPECTED_PAGE_TITLE) throws IOException {
+
+        // Getting page title and storing as String
+        String actualPageTitle = WebDriverUtils.driver.getTitle();
+
+        // Assertion to check if actual page title is the same as expected page title
+        Assert.assertTrue(actualPageTitle.contentEquals(ApplicationConstants.EXPECTED_PAGE_TITLE));
+
         // Attaching screenshot to Extent report
-        try {
-            CucumberLogUtils.logExtentScreenshot();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        CucumberLogUtils.logExtentScreenshot();
 
         // Attaching screenshot to Cucumber HTML report
         CucumberLogUtils.logScreenShot();
